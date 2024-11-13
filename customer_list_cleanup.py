bf0a5ce12571b3b999fa20b26f90cdbf3543c2ff
Craig Bowman <cbowman@appdev.yearup.org>
2024-11-12 23:17:11 -0600

    customer list cleanup columns seperated cust_id set to index, markdown comments need to added

--- customer_list_cleanup.py
+++ customer_list_cleanup.py
@@ -7,3 +7,6 @@ customer_list = pd.read_csv('customer_list_updated.csv')
 
 print(customer_list.head())
 print(customer_list.info())
+
+
+customer_list[['cust_id','date','time','name','email','phone','sms-opt-out']] = customer_list['cust_id|date|time|name|email|phone|sms-opt-out'].str.split('|')
